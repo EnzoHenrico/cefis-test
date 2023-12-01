4d26d46f7a9d6798524f03a724052ac384b5d04c
EnzoHenrico <enzo.mascia1@gmail.com>
2023-12-01 14:33:20 -0300

    Created DB seed with prisma

---
+++ prisma/data/Course.ts
@@ -0,0 +1,27 @@
+interface InsertCourse {
+  title: string;
+  description: string;
+  teacherId: number;
+  lengthInHours: number;
+}
+
+export const Courses: InsertCourse[] = [
+  {
+    title: 'Introdução a IoT',
+    description: 'Introdução aos conceito de Internet das coisas e desenvolvimento de programas para dispositivs embarcados.',
+    teacherId: 3,
+    lengthInHours: 60,
+  },
+  {
+    title: 'Administração de processos de TI',
+    description: 'Conceitos avançados de administração para equipes de TI em desenvolvimento no mercado.',
+    teacherId: 3,
+    lengthInHours: 60,
+  },
+  {
+    title: 'Contabilidade',
+    description: 'Do básico ao avançado no mundo da contabilidade.',
+    teacherId: 4,
+    lengthInHours: 60,
+  },
+]
---
+++ prisma/data/Question.ts
@@ -0,0 +1,47 @@
+
+interface InsertQuestion {
+  content: string;
+  createdBy: string;
+  replyId?: number;
+  courseId: number;
+  replied: boolean;
+}
+
+export const Questions: InsertQuestion[] = [
+  {
+    content: 'Como a conectividade entre dispositivos na Internet das Coisas facilita o nosso cotidiano?',
+    createdBy: 'Julia Ruiz',
+    courseId: 1,
+    replied: false,
+  },
+  {
+    content: 'Qual é a diferença entre contabilidade financeira e contabilidade gerencial? Como essas duas áreas se diferenciam em termos de propósito, audiência e métodos de relatórios financeiros?',
+    createdBy: 'Enzo Henrico',
+    courseId: 2,
+    replied: false,
+  },
+  {
+    content: 'Como a implementação de frameworks como o COBIT (Control Objectives for Information and Related Technologies) ou o ITIL (Information Technology Infrastructure Library) pode melhorar a eficiência e a qualidade dos processos de TI em uma organização? Quais são os principais desafios na adoção desses frameworks?',
+    createdBy: 'Julia Ruiz',
+    courseId: 3,
+    replied: false,
+  },
+  {
+    content: 'Quais tipos de dispositivo existem?',
+    createdBy: 'Enzo Henrico',
+    courseId: 1,
+    replied: false,
+  },
+  {
+    content: 'A contabilidade é aplicavel somente em empresas ou também no nosso dia a dia?',
+    createdBy: 'Julia Ruiz',
+    courseId: 2,
+    replied: false,
+  },
+  {
+    content: 'E essencial a inclusão de toda a equipe de TI, até mesmo os menos experientes, tomada de decisões sobre novas tecnologias no projeto da empresa em um contexto de escalabilidade?',
+    createdBy: 'Enzo Henrico',
+    courseId: 3,
+    replied: false,
+  },
+]

37edc5aed6378da5d4152edb53a6b1d6e2913f71
EnzoHenrico <enzo.mascia1@gmail.com>
2023-12-04 02:32:32 -0300

    remove import

--- packages/client/prisma/seed.ts
+++ packages/client/prisma/seed.ts
@@ -1,9 +1,9 @@
-import { Users } from "./data/User";
-import { Courses } from "./data/Course";
-import { Questions } from "./data/Question";
-import { PrismaClient, Reply } from "@prisma/client";
+import { Users } from "./data/User"
+import { Courses } from "./data/Course"
+import { Questions } from "./data/Question"
+import { PrismaClient } from "@prisma/client"
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 async function seed() {
   try {
@@ -11,30 +11,30 @@ async function seed() {
     Users.map(async (user) => {
       await prisma.user.create({
         data: user,
-      });
-    });
+      })
+    })
 
     // Seed Course table
     Courses.map(async (course) => {
       await prisma.course.create({
         data: course,
-      });
-    });
+      })
+    })
 
     // Seed Question table
     Questions.map(async (question) => {
       await prisma.question.create({
         data: question,
-      });
-    });
+      })
+    })
 
-    await prisma.$disconnect();
+    await prisma.$disconnect()
   } catch (error) {
-    console.error(error);
-    await prisma.$disconnect();
-    process.exit(1);
+    console.error(error)
+    await prisma.$disconnect()
+    process.exit(1)
   }
 }
 
 // Run seed
-seed();
+seed()
